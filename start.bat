d3b1d541ed698f1e000c75d164897c074a732ae4
FerhatYesilkaya <ferhat-20001@hotmail.de>
2024-10-11 18:46:15 +0200

    added functionality to decide between x64 and x86

--- start.bat
+++ start.bat
@@ -1,4 +1,11 @@
 @echo off
 set "dateipfad=%~dp0Configuration.au3"
-set "programm=%~dp0install\AutoIt3_x64.exe"
+
+rem Prüfen, ob es ein 64-Bit-System ist
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set "programm=%~dp0install\AutoIt3_x64.exe"
+) else (
+    set "programm=%~dp0install\AutoIt3.exe"
+)
+
 start "" "%programm%" "%dateipfad%"
